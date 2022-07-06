848ea9ca8a955a8cf86bb466d126326641f4e830
Prateek Rath <prateekrath28@gmail.com>
2022-07-06 21:46:05 +0530

    deploy to mongo Atlas

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true})
+mongoose.connect("mongodb+srv://admin-prateek:Prateek8@cluster0.epb4t.mongodb.net/todolistDB", { useNewUrlParser: true})
 const itemsSchema = new mongoose.Schema({
   name:String
 })
